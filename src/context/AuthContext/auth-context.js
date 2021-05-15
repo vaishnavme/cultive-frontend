835dc2acfd31a8aae96fd430d57d0f5a33a96572
vaishnavme <42497931+vaishnavme@users.noreply.github.com>
2021-05-15 06:00:39 +0530

    auth context

---
+++ src/context/AuthContext/auth-context.js
@@ -0,0 +1,45 @@
+import { createContext, useContext, useState } from "react";
+import axios from "axios"
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(
+        JSON.parse(localStorage.getItem("authUser"))
+    )
+
+    const loginCredentialHandler = async (email, password) => {
+        console.log("ema", email)
+        try {
+            const { data: {data, success} } = await axios.post("user/login", {
+                email,
+                password
+            })
+            console.log("user", data)
+            if(success) {
+                setUser(data);
+                localStorage?.setItem("authUser", JSON.stringify(data));
+            }
+            return { data, success };
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    const logOutUser = async () => {
+        localStorage?.removeItem("authUser");
+        setUser(null);
+    }
+
+    return (
+        <AuthContext.Provider value={{
+            user, 
+            loginCredentialHandler,
+            logOutUser
+        }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export const useAuth = () => useContext(AuthContext);
