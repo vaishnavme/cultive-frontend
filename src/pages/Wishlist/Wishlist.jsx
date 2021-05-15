95519c94f8ae66d7d75db4f750f14f3c3d078ef4
vaishnavme <42497931+vaishnavme@users.noreply.github.com>
2021-05-15 05:48:47 +0530

    Wishlist page and style

---
+++ src/pages/Wishlist/Wishlist.jsx
@@ -0,0 +1,31 @@
+import { useNavigate } from "react-router-dom";
+import { useData } from "../../context";
+import { ProductCard } from "../../components";
+import styles from "./Wishlist.module.css";
+
+export default function Wishlist() {
+    const navigate = useNavigate();
+    const { wishListItems } = useData();
+    
+    return (
+        <div className={`${styles.main}`}>
+            {
+                !!!wishListItems.length && (
+                    <div className={`${styles.notify}`}>
+                        <div className={`h3`}>Empty Wishlist!!</div>
+                        <p>You have no item in wishlist.</p>
+                        <button className={`btn btn-secondary`} onClick={() => navigate("/products")}>Start Adding</button>
+                    </div>
+                )
+            }
+            <div className={`${styles.productGrid} mt-4`}>
+                {
+                    wishListItems &&
+                    wishListItems.map((item) => (
+                        <ProductCard key={item._id} product={item}/>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
