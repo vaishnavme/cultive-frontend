935fece517a3f1f025fa4b54b4a1299e88b53ad6
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-06-20 11:57:21 +0530

    Banner section image added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,26 @@
 import { Fragment } from "react";
 import { Carousel } from "../../components";
+import styles from "./Home.module.css";
 
 export default function Home() {
     return (
         <Fragment>
             <Carousel/>
+            <section className={`${styles.bannerSection}`}>
+                <div className={`${styles.banner}`}>
+                    <img 
+                        className={`${styles.bannerImg}`}
+                        src="https://cdn.shopify.com/s/files/1/0317/0687/3992/files/banner-v4-1.jpg" 
+                        alt="indoor"/>
+                </div>
+                <div className={`${styles.banner}`}>
+                    <img 
+                        className={`${styles.bannerImg}`}
+                        src="https://cdn.shopify.com/s/files/1/0317/0687/3992/files/banner-v4-2.jpg" 
+                        alt="indoor"/>
+                </div>
+            </section>
         </Fragment>
     )
 }
+
