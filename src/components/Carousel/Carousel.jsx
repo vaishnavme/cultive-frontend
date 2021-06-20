cd391e584fbff499eb1716092585189e3e87674a
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-06-20 10:32:13 +0530

    Basic carousel complete

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import  {carouseldata} from "./carouselData";
+import styles from "./Carousel.module.css";
+
+export const Carousel = () => {
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const slideLength = carouseldata.length;
+    
+    const nextSlide = () => {
+        setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1)
+    }
+
+    const prevSlide = () => {
+        setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1)
+    }
+
+    return (
+        <section className={`${styles.carousel}`}>
+            <button 
+                onClick={nextSlide}
+                className={`btn ${styles.carouselBtn} ${styles.right}`}>
+                <i className='bx bx-right-arrow'></i>
+            </button>
+            <button 
+                onClick={prevSlide}
+                className={`btn ${styles.carouselBtn} ${styles.left}`}>
+                <i className='bx bx-left-arrow'></i>
+            </button>
+            {
+                carouseldata.map((slide, index) => (
+                    <div 
+                        key={index}
+                        className={`${index} === ${currentSlide} ? ${styles.activeSlide} : ${styles.slide}`}>
+                        { index === currentSlide &&
+                            <img 
+                            className={`${styles.slideImage}`} 
+                            src={slide.imageUrl} 
+                            alt={`slide-${index}`}
+                        />}
+                    </div>
+                ))
+            }
+        </section>
+    )
+}
