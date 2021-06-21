40a61f4caff1896f7e6fec573b9323a1cba7eb81
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-06-21 20:03:54 +0530

    Cart page layour update

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -9,91 +9,110 @@ export default function Cart() {
     const totalPrice = (items) => {
         return items.reduce((total, { price, quantity }) => total + price * quantity, 0);
     }
+    const cartTotal = totalPrice(cartItems) || 0;
 
     const wishBtnHandler = (productID) => {
         addToWishlist({product: productID});
         dispatch({type: "REMOVE_FROM_CART", payload: productID})
     }
 
+    console.log("cartItems: ", cartItems)
+
     return (
-        <>
-        {
-            !!!cartItems.length ? (
-                <div className={`${styles.notify}`}>
-                    <div className={`h3`}>Empty Cart!!</div>
-                    <p>You have no item in Cart.</p>
-                    <button className={`btn btn-secondary`} onClick={() => navigate("/products")}>Let's Shop</button>
-                </div>
-            ) : (
-                <div className={`${styles.main}`}>
-            <div className={`${styles.cart}`}>
-            <ul className={`simple-list ${styles.item}`}>
-                {
-                    cartItems &&
-                    cartItems.map((item) => (               
-                            <li key={item._id} className={`d-flex pb-2 ${styles.listItem}`}>
-                                <img className={styles.itemImage} src={item.image} alt={item.name}/>
-                                <div className={`ml-3`}>
-                                    <div className={`h5`}>{item.name}</div>
-                                    <div className={`h6`}>Rs. {item.price}</div>
-
-                                    <div className={`d-flex flex-align-center mt-2`}>
-                                        <button 
-                                            className={`btn iconBtn`}
-                                            disabled={item.quantity < 2 ? true : false} 
-                                            onClick={() => quantityHandler({
-                                                type: "DEC_QNT",
-                                                product: item._id,
-                                                quantity: item.quantity
-                                            })}>
-                                                <i class='bx bx-minus' ></i>
-                                        </button>
-                                        <div className={`h6`}>Quantity: {item.quantity}</div>
-                                        <button 
-                                            className={`btn iconBtn`} 
-                                            onClick={() => quantityHandler({
-                                                type: "INC_QNT",
-                                                product: item._id,
-                                                quantity: item.quantity
-                                            })}>
-                                                <i class='bx bx-plus' ></i>
-                                        </button>
+        <section className={`${styles.cartSection}`}>
+            {
+                !cartItems.length ? 
+                (
+                    <div className={`${styles.centerBlock}`}>
+                        <div>Your Cart is empty!</div>
+                        <button onClick={() => navigate("/products")} className={`btn btn-secondary`}>Let's Shop</button>
+                    </div>
+                ) : (
+                    <div>
+                        <div className={`h4`}>Your Cart</div>
+                        <div className={`${styles.container}`}>
+                            <div className={`${styles.cartItems}`}>
+                                <ul>
+                                    {
+                                        cartItems?.map((item) => (
+                                            <li key={item._id} className={`${styles.productList}`}>
+                                                <div className={`${styles.product}`}>
+                                                    <div className={`${styles.productInfo}`}>
+                                                        <img className={`${styles.productImage}`} src={item.image} alt={item.name}/>
+                                                        <div className={`ml-2`}>{item.name}</div>    
+                                                    </div>
+                                                    <div className={`${styles.productQuantity}`}>
+                                                        <button 
+                                                            onClick={() => quantityHandler({
+                                                            type: "INC_QNT",product: item._id, quantity: item.quantity})} className={`btn ${styles.btnIcon}`}><i class='bx bx-plus'></i></button>
+                                                        <span className={`ml-2 mr-2 h6`}>{item.quantity}</span>
+                                                        <button 
+                                                            disabled={item.quantity < 2 ? true : false} 
+                                                            onClick={() => quantityHandler({
+                                                            type: "DEC_QNT",product: item._id,quantity: item.quantity})} className={`btn ${styles.btnIcon}`}><i class='bx bx-minus' ></i></button>
+                                                    </div>
+                                                    <div className={`${styles.productPrice} h6`}>
+                                                        {item.price} ₹
+                                                    </div>
+                                                    <div className={`${styles.productAction}`}>
+                                                        <button onClick={() => wishBtnHandler(item._id)} className={`btn ${styles.btnIcon}`}><i class='bx bx-heart'></i></button>
+                                                        <button onClick={() => removeFromCart({product: item._id})} className={`btn ${styles.btnIcon}`}><i class='bx bx-x'></i></button>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                        ))
+                                    }
+                                </ul>
+                            </div>
+                            <div className={`${styles.checkout}`}>
+                                <div className={`${styles.cardCheck}`}>
+                                    <label className={`${styles.lableName}`}>Payment Type</label>
+                                    <div className={`${styles.select}`}>
+                                        <div className={`${styles.btnSelect}`} role="button"><i class='bx bxl-visa' ></i></div>
+                                        <div className={`${styles.btnSelect}`}><i class='bx bxl-paypal' ></i></div>
                                     </div>
 
-                                    <div className={`mt-2`}>
-                                        <button 
-                                            className={`btn ${styles.actionBtn}`}
-                                            onClick={() => removeFromCart({product: item._id})}>REMOVE
-                                        </button>
-                                        <button className={`btn ${styles.actionBtn}`}
-                                        onClick={() => wishBtnHandler(item._id)}>
-                                            MOVE TO WISHLIST
-                                        </button> 
+                                    <div className={`${styles.formGroup} mt-2`}>
+                                        <label className={`${styles.lableName}`}>Name on Card</label>
+                                        <input type="text" className={`${styles.inputField} ${styles.inputFieldBlock}`} placeholder="Your Name"/>
+
+                                        <div className={`${styles.cardDetails}`}>
+                                            <div>
+                                                <label className={`${styles.lableName}`}>Expiration Date</label>
+                                                <input type="text" className={`${styles.inputField} ${styles.inputField}`} placeholder="MM/YY"/>
+                                            </div>
+                                            <div>
+                                                <label className={`${styles.lableName}`}>CVV</label>
+                                                <input type="text" className={`${styles.inputField} ${styles.inputField}`} placeholder="eg. 123"/>
+                                            </div>
+                                        </div>
                                     </div>
+                                    <div className={`${styles.billing}`}>
+                                        <div className={`${styles.billingLabel}`}>
+                                            <ul>
+                                                <li>Subtotal</li>
+                                                <li>Shipping</li>
+                                                <li>Total (Tax incl.)</li>
+                                            </ul>
+                                        </div>
+                                        <div className={`${styles.billingTotal}`}>
+                                            <ul>
+                                                <li>Subtotal</li>
+                                                <li>Shipping</li>
+                                                <li>{cartTotal}</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <button className={`${styles.btnBlock}`}>
+                                        <div>{cartTotal}</div>
+                                        <div className={`d-flex flex-align-center`}>Checkout <i class='bx bxs-right-arrow-alt h5' ></i></div>
+                                    </button>
                                 </div>
-                            </li>
-                    ))
-                }
-                </ul>
-            </div>
-            <div className={`${styles.checkout}`}>
-                <div className={`h5`}>Checkout</div>
-                <ul>
-                    {
-                        cartItems.map((item) => (
-                            <li key={item.name}>{item.name}</li>
-                        ))
-                    }
-                </ul>
-                <div className={`d-flex flex-justify-space-between flex-align-center ${styles.bill}`}>
-                    <div>Total</div>
-                    <div>Rs {totalPrice(cartItems)}</div>
-                </div>
-            </div>
-        </div>
-            )
-        }
-        
-        </>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+        </section>  
     )
 }
