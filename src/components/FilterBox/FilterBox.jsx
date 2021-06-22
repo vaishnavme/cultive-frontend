732d6dcad351bb8a6d010c20f5fa99db008f9526
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-06-22 11:38:08 +0530

    Filter box component created

--- src/components/FilterBox/FilterBox.jsx
+++ src/components/FilterBox/FilterBox.jsx
@@ -1,3 +1,83 @@
+import { useState } from "react";
+
+// import styles from "./FilterBox.module.css";
+
+// export const FilterBox = (
+//     {dispatch, 
+//     showInventoryAll, 
+//     sortBy, 
+//     categoryList, 
+//     categories
+//     }) => {
+
+
+//     return (
+//         <>
+//             <div className={`${styles.sidebar}`}>
+//                 <div className={`${styles.card} mt-4`}>
+//                     <div className={`d-flex flex-justify-space-between flex-align-center p-1 pb-2 mb-2 ${styles.cardSection}`}>
+//                         <h5>FILTER</h5>
+//                         <button
+//                             onClick={() => dispatch({type: "CLEAR_ALL_FILTERS"})}
+//                             className={`btn ${styles.btn}`}>
+//                             CLEAR
+//                         </button>
+//                     </div>
+//                     <div className={`p-1 pb-2 mb-2 ${styles.cardSection}`}>
+//                         <h6 className={`mt-1 mb-1`}>Categories</h6>
+//                         <ul>
+//                             {
+//                                 categoryList.map((category) => (
+//                                 <li 
+//                                     key={category}
+//                                     className={`d-flex flex-justify-space-between flex-align-center`}>
+//                                     <label>{category}</label>
+//                                     <input 
+//                                         type="checkbox"
+//                                         checked={categories.some(value => value === category)} 
+//                                         onChange={() => dispatch({type: "TOGGLE_CATEGORY", payload: category})}
+//                                         value={category}/>
+//                                 </li>
+//                                 ))
+//                             }
+//                         </ul>
+//                     </div>
+
+//                     <div className={`p-1 pb-2 mb-2`}>
+//                         <h6 className={`mt-1 mb-1`}>Sort By</h6>
+//                         <ul>
+//                             <li className={`d-flex flex-justify-space-between flex-align-center`}>
+//                                 <label htmlFor="lowTohigh">Price: Low to High</label>
+//                                 <input 
+//                                     type="radio" 
+//                                     checked={sortBy && sortBy === "PRICE_LOW_TO_HIGH"}
+//                                     onChange={() => dispatch({type: "SORT", payload: "PRICE_LOW_TO_HIGH"})}
+//                                 />
+//                             </li>
+//                             <li className={`d-flex flex-justify-space-between flex-align-center`}>
+//                                 <label htmlFor="hightToLow">Price: High to Low</label>
+//                                 <input 
+//                                     type="radio" 
+//                                     checked={sortBy && sortBy === "PRICE_HIGH_TO_LOW"}
+//                                     onChange={() => dispatch({type: "SORT", payload: "PRICE_HIGH_TO_LOW"})}
+//                                 />
+//                             </li>
+//                             <li className={`d-flex flex-justify-space-between flex-align-center`}>
+//                                 <label htmlFor="tropical">Include Out of Stock</label>
+//                                 <input 
+//                                     type="checkbox"
+//                                     checked={showInventoryAll}
+//                                     onChange={() => dispatch({type: "TOGGLE_INVENTORY"})}
+//                                 /> 
+//                             </li>
+//                         </ul>
+//                     </div>
+//                 </div>
+//             </div>
+//         </>
+//     )
+// }
+
 import styles from "./FilterBox.module.css";
 
 export const FilterBox = (
@@ -7,71 +87,64 @@ export const FilterBox = (
     categoryList, 
     categories
     }) => {
+        const [isVisible, setVisible] = useState(false);
 
+        const setFilterBoxVisible = () => setVisible((prevState) => !prevState)
+    
+        const filterBoxStyle = {
+            borderRadius: "0.2rem",
+            backgroundColor: "#f5f5ff",
+            height: isVisible ? "auto" : 0,
+            opacity: isVisible ? 1 : 0,
+            transition: "all 800ms ease-in"
+        }
 
     return (
-        <>
-            <div className={`${styles.sidebar}`}>
-                <div className={`${styles.card} mt-4`}>
-                    <div className={`d-flex flex-justify-space-between flex-align-center p-1 pb-2 mb-2 ${styles.cardSection}`}>
-                        <h5>FILTER</h5>
-                        <button
-                            onClick={() => dispatch({type: "CLEAR_ALL_FILTERS"})}
-                            className={`btn ${styles.btn}`}>
-                            CLEAR
-                        </button>
+        <div className={`${styles.wrapper}`}>
+                <div className={`${styles.action}`}>
+                    <button onClick={setFilterBoxVisible} className={`${styles.btnIcon}`}><i class='bx bx-filter-alt mr-1 h4'></i> FILTER</button>
+
+                    <button className={`${styles.btnIcon} ml-4`}>CLEAR</button>
+                    
+                    <div className={`${styles.actionSelect}`}>
+                        <select className={`${styles.selectOption}`}>
+                            <option value="volvo">Best Seller</option>
+                            <option value="audi">Featured</option>
+                            <option value="saab">Price, Low to High</option>
+                            <option value="vw">Price, High to Low</option>
+                        </select>
                     </div>
-                    <div className={`p-1 pb-2 mb-2 ${styles.cardSection}`}>
-                        <h6 className={`mt-1 mb-1`}>Categories</h6>
-                        <ul>
+
+                </div>
+                <div style={filterBoxStyle}>
+                    <div className={`${styles.filterBox}`}>
+                        <div className={`${styles.box}`}>
+                            <div className={`${styles.filterName}`}>CATEGORIES</div>
+                            <ul className={`mt-2 mb-2`}>
                             {
                                 categoryList.map((category) => (
                                 <li 
                                     key={category}
-                                    className={`d-flex flex-justify-space-between flex-align-center`}>
-                                    <label>{category}</label>
-                                    <input 
-                                        type="checkbox"
-                                        checked={categories.some(value => value === category)} 
-                                        onChange={() => dispatch({type: "TOGGLE_CATEGORY", payload: category})}
-                                        value={category}/>
-                                </li>
-                                ))
-                            }
-                        </ul>
-                    </div>
-
-                    <div className={`p-1 pb-2 mb-2`}>
-                        <h6 className={`mt-1 mb-1`}>Sort By</h6>
-                        <ul>
-                            <li className={`d-flex flex-justify-space-between flex-align-center`}>
-                                <label htmlFor="lowTohigh">Price: Low to High</label>
-                                <input 
-                                    type="radio" 
-                                    checked={sortBy && sortBy === "PRICE_LOW_TO_HIGH"}
-                                    onChange={() => dispatch({type: "SORT", payload: "PRICE_LOW_TO_HIGH"})}
-                                />
-                            </li>
-                            <li className={`d-flex flex-justify-space-between flex-align-center`}>
-                                <label htmlFor="hightToLow">Price: High to Low</label>
-                                <input 
-                                    type="radio" 
-                                    checked={sortBy && sortBy === "PRICE_HIGH_TO_LOW"}
-                                    onChange={() => dispatch({type: "SORT", payload: "PRICE_HIGH_TO_LOW"})}
-                                />
-                            </li>
-                            <li className={`d-flex flex-justify-space-between flex-align-center`}>
-                                <label htmlFor="tropical">Include Out of Stock</label>
+                                    className={`d-flex flex-align-center`}>
                                 <input 
                                     type="checkbox"
-                                    checked={showInventoryAll}
-                                    onChange={() => dispatch({type: "TOGGLE_INVENTORY"})}
-                                /> 
+                                    checked={categories.some(value => value === category)} 
+                                    onChange={() => dispatch({type: "TOGGLE_CATEGORY", payload: category})}
+                                    value={category}/>
+                                <label className={`${styles.labelName}`}>{category}</label>
                             </li>
+                            ))
+                        }
                         </ul>
+                    </div>
+                    <div className={`${styles.box}`}>
+                        <div className={`${styles.filterName}`}>PRICE</div>
+                    </div>
+                    <div className={`${styles.box}`}>
+                        <div className={`${styles.filterName}`}>RATING</div>
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
