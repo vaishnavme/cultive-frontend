172622661cde39c1d4ade4cd5809ea46f7940c3a
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-06-22 11:38:19 +0530

    Product module style update

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useData } from "../../context";
 import { ProductCard, FilterBox, onlyUnique, Loader } from "../../components";
 import styles from "./Products.module.css";
 
 export default function Products() {
-    const [isFilterVisible, setFilterVisible] = useState(false);
+    const [isVisible, setVisible] = useState(false);
     const { productData, showInventoryAll, sortBy, categories, dispatch, isLoading } = useData();
     
     const categoryList = productData.map((property) => property.category).filter(onlyUnique);
@@ -30,60 +30,28 @@ export default function Products() {
     const sortedData = getSortedData(productData, sortBy);
     const filteredData = getFilteredData(sortedData, {showInventoryAll, categories});
     
+    const setFilterBoxVisible = () => setVisible((prevState) => !prevState)
     
-
+    const filterBoxStyle = {
+        borderRadius: "0.2rem",
+        backgroundColor: "#f5f5ff",
+        height: isVisible ? "auto" : 0,
+        opacity: isVisible ? 1 : 0,
+        transition: "all 800ms ease-in"
+    }
     
     return (
         <section>
             <div className={`${styles.header}`}>
                 <div className={`${styles.headerText} h1`}>Products</div>
             </div>
-            <div className={`${styles.wrapper}`}>
-                <div className={`${styles.action}`}>
-                    <button className={`${styles.btnIcon}`}><i class='bx bx-filter-alt mr-1 h4'></i> FILTER</button>
-
-                    <div className={`${styles.actionSelect}`}>
-                        <select className={`${styles.selectOption}`}>
-                            <option value="volvo">Best Seller</option>
-                            <option value="audi">Featured</option>
-                            <option value="saab">Price, Low to High</option>
-                            <option value="vw">Price, High to Low</option>
-                        </select>
-                    </div>
-
-                    <button className={`${styles.btnIcon} ml-4`}>CLEAR</button>
-
-                </div>
-                <div className={`${styles.collapse}`}>
-                    <div className={`${styles.filterBox}`}>
-                        <div className={`${styles.box}`}>
-                            <div className={`${styles.filterName}`}>CATEGORIES</div>
-                            <ul className={`mt-2 mb-2`}>
-                            {
-                                categoryList.map((category) => (
-                                <li 
-                                    key={category}
-                                    className={`d-flex flex-align-center`}>
-                                    <input 
-                                        type="checkbox"
-                                        checked={categories.some(value => value === category)} 
-                                        onChange={() => dispatch({type: "TOGGLE_CATEGORY", payload: category})}
-                                        value={category}/>
-                                    <label className={`${styles.labelName}`}>{category}</label>
-                                </li>
-                                ))
-                            }
-                            </ul>
-                        </div>
-                        <div className={`${styles.box}`}>
-                            <div className={`${styles.filterName}`}>PRICE</div>
-                        </div>
-                        <div className={`${styles.box}`}>
-                            <div className={`${styles.filterName}`}>RATING</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <FilterBox 
+                showInventoryAll={showInventoryAll}
+                categoryList={categoryList}
+                categories={categories}
+                sortBy={sortBy}
+                dispatch={dispatch}
+            />
         </section>
     )
 }
