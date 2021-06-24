e56e7c4f8606fedb41f3a37e6c9a0727bea424fa
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-06-24 13:25:45 +0530

    bug fix

--- src/context/AuthContext/auth-context.js
+++ src/context/AuthContext/auth-context.js
@@ -26,7 +26,7 @@ export const AuthProvider = ({children}) => {
         }
     }
 
-    const logOutUser = async () => {
+    const logOutUser = () => {
         localStorage?.removeItem("authUser");
         setUser(null);
     }
